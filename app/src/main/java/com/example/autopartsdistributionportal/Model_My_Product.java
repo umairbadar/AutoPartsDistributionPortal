554869e29b519c36bb602d7d2a6648023625d343
Developer <syedumairbadar@gmail.com>
2019-06-28 11:40:30 +0500

    Final Version

--- app/src/main/java/com/example/autopartsdistributionportal/Model_My_Product.java
+++ app/src/main/java/com/example/autopartsdistributionportal/Model_My_Product.java
@@ -3,19 +3,25 @@ package com.example.autopartsdistributionportal;
 public class Model_My_Product {
 
     private String p_id;
+    private String product_id;
     private String p_name;
     private String p_img;
     private String p_status;
     private String p_unit_price;
     private String quantity;
 
-    public Model_My_Product(String p_id, String p_name, String p_img, String p_status, String p_unit_price, String quantity) {
+    public Model_My_Product(String p_id,String product_id, String p_name, String p_img, String p_status, String p_unit_price, String quantity) {
         this.p_id = p_id;
+        this.product_id = product_id;
         this.p_name = p_name;
         this.p_img = p_img;
         this.p_status = p_status;
         this.p_unit_price = p_unit_price;
         this.quantity = quantity;
+    }
+
+    public String getProduct_id() {
+        return product_id;
     }
 
     public String getP_id() {
